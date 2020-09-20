75fb86638ce37bea342b36dabe4d72259c5c51f1
Xchkoo <root@XchoodeMacBook-Pro.local>
2020-09-20 09:10:41 +0800

    change

--- luoGuCode/p1439/p1439.cpp
+++ luoGuCode/p1439/p1439.cpp
@@ -1,31 +1,37 @@
 #include<iostream>
 #include<string>
+#define MAXNUM 100005
 using namespace std;
-#define MAXNUM 100000
-int p1[MAXNUM],p2[MAXNUM];
 int n;
-int f[MAXNUM][MAXNUM];
+int p1[MAXNUM],p2[MAXNUM];
+int f[MAXNUM];
+
+int read()
+{
+    int x=0,f=1;char ch=getchar();
+    while (ch<'0' || ch>'9'){if (ch=='-')f=-1;ch=getchar();}
+    while ('0'<=ch && ch<='9'){x=(x<<3)+(x<<1)+(ch^48);ch=getchar();}
+    return x*f;
+}
 int main()
 {
-	cin >> n;
+	n = read();
 	memset(p1,0,sizeof(p1));
 	memset(p2,0,sizeof(p2));
 	for(int i = 1;i <= n;i++)
-		cin >> p1[i];
+		p1[i] = read();
 	for(int i = 1;i <= n;i++)
-		cin >> p2[i];
-//input
-	for(int i = 0;i <= n;i++)
-	{
-		for(int j = 0;j <= n;j++)
+		p2[i] = read();
+		for(int i = 0;i <= n;i++)
 		{
-			if(i == 0 || j == 0)	f[i][j] = 0;
-			else if(p1[i] == p2[i])	f[i][j] = f[i - 1][j - 1] + 1;
-			else f[i][j] = max(f[i - 1][j],f[i][j - 1]);
+			for(int j = 0;j <= n;j++)
+			{
+				if(i == 0 || j == 0)	f[j] = 0;
+				else if(p1[i] == p2[j])	f[j] = f[j - 1] + 1;
+				else if(p1[i] != p2[j])f[j] = max(f[j - 1],f[j]);
+			}
 		}
-	}
-	
-	cout << f[n][n] <<endl;
-	return 0;
+		
+		cout << f[n] << endl;
+		return 0;
 }
-
