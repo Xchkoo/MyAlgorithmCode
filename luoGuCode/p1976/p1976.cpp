5541a715ad1ca24ba25c4e40933ead45c9046a96
Xchkoo <Xchkoo@foxmail.com>
2020-09-27 09:41:55 +0800

    add

--- luoGuCode/p1976/p1976.cpp
+++ luoGuCode/p1976/p1976.cpp
@@ -1,12 +1,17 @@
-#include<iostream>
+nclude<iostream>
 using namespace std;
-
-unsigned long long ctl[32768:;
-
+typedef unsigned long long ull;
+int i,j,k,n;
+ull l[10000];
+#define largeN 100000007
 int main(){
 	cin >> n;
-	
-	return 0;
-}
-//ctl[i] += ctl[j] * ctl[i - j - 1];
-//h(n)=C(2n,n)/(n+1)
+	l[0] = 1;
+	l[1] = 1;
+	for(i=2;i<=n;i++)
+		for(j=0;j<i;j++)
+			l[i] = (l[j]*l[i-j-1]%largeN+l[i])%largeN;
+		cout<<l[n];										return 0;										}
+											//ctl[i] += ctl[j] * ctl[i - j - 1];
+											//h(n)=C(2n,n)/(n+1)
+
