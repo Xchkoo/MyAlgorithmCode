217c8fa9c2df14f795a60409239173e3566dc084
Xchkoo <Xchkoo@foxmail.com>
2020-10-18 10:33:44 +0800

    myowncode

--- luoGuCode/p3366/p3366.cpp
+++ luoGuCode/p3366/p3366.cpp
@@ -1,75 +1,62 @@
-#include<cstdio>
-#include<cstdlib>
 #include<iostream>
-
 using namespace std;
-/*最小生成树Prim未优化版*/
 
-int book[100];//用于记录这个点有没有被访问过
-int dis[100];//用于记录距离树的距离最短路程
-const int MAX = 0x3ffff;//边界值
-int maps[100][100];//用于记录所有边的关系
+const int INF = 0x3ffff;
+
+int n,m,x,y,z;//m=edgenumber n=pointnumber
+bool book[5001];
+int _map[5001][5001],dis[5001];
+int _min,_minIndex;
+int sum;
+void prim()
+{
+	//先从第一个点开始
+	for(int i = 1;i<=n;i++)
+	{
+		dis[i] = _map[1][i];
+	}
+	book[1] = 1;
+	for(int i = 1;i<=n-1;i++)//开始循环 
+	{
+		_min = INF;
+		for(int j = 1;j<=n;j++)
+		{
+			if(_min>dis[j]&&book[j] == 0)
+			{
+				_min = dis[j];
+				_minIndex = j;
+			}
+		}
+		book[_minIndex] = 1;
+		sum += dis[_minIndex];//目的是求最小生成树的长度
+		
+		for(int j = 1;j<=n;j++)//求没被选上的
+		{
+			if(book[j] == 0 && _map[_minIndex][j] < dis[j])
+				dis[j] = _map[_minIndex][j];	
+		} 
+	}
+}
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    int i,j,k;//循环变量
-    int n,m;//输入的N个点，和M条边
-    int x,y,z;//输入变量
-    int min,minIndex;
-    int sum=0;//记录最后的答案
-    
-    cin>>n>>m;
-
-    //初始化maps，除了自己到自己是0其他都是边界值
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n; j++)
-        {
-            if(i!=j)
-                maps[i][j] = MAX;
-            else
-                maps[i][j] = 0;
-        }
-    }
-            
-    for (i = 1; i <= m; i++)
-    {
-        cin>>x>>y>>z;//输入的为无向图
-        maps[x][y] = z;
-        maps[y][x] = z;
-    }
-
-    //初始化距离数组，默认先把离1点最近的找出来放好
-    for (i = 1; i <= n; i++)
-        dis[i] = maps[1][i];
-
-    book[1]=1;//记录1已经被访问过了
-
-    for (i = 1; i <= n-1; i++)//1已经访问过了，所以循环n-1次
-    {
-        min = MAX;//对于最小值赋值，其实这里也应该对minIndex进行赋值，但是我们承认这个图一定有最小生成树而且不存在两条相同的边
-        //寻找离树最近的点
-        for (j = 1; j <= n; j++)
-        {
-            if(book[j] ==0 && dis[j] < min)
-            {
-                min = dis[j];
-                minIndex = j;
-            }
-        }
-
-        //记录这个点已经被访问过了
-        book[minIndex] = 1;
-        sum += dis[minIndex];
-
-        for (j = 1; j <= n; j++)
-        {
-            //如果这点没有被访问过，而且这个点到任意一点的距离比现在到树的距离近那么更新
-            if(book[j] == 0 && maps[minIndex][j] < dis[j])
-                dis[j] = maps[minIndex][j];
-        }
-    }
-
-    cout<<sum<<endl;
+	std::ios::sync_with_stdio(false);
+	cin >> n >> m;
+	for(int i = 1;i<=n;i++)
+		for(int j = 1;j<=n;j++)
+		{
+		if(i != j)
+			_map[i][j] = INF;
+		else
+			_map[i][j]=_map[j][i] = 0;
+		}
+	for(int i = 1;i <= m;i++)
+	{
+		cin >> x >> y >> z;
+		_map[x][y]=_map[y][x]=z;
+	}
+	prim();
+	cout<<sum;
+	return 0;
 }
+
