8ab2635bd5efd74f77b86bbf3edf4cef9652af70
Xchkoo <Xchkoo@foxmail.com>
2020-10-21 18:57:01 +0800

    final

--- luoGuCode/p4779(dijkstra)/p4779.cpp
+++ luoGuCode/p4779(dijkstra)/p4779.cpp
@@ -1,22 +1,84 @@
 #include<iostream>
+#include<queue>
 using namespace std;
-#define MAX 100000
-int dis[MAX];head[MAX];
-struct edge{
-	int next,to,value;
+const int INF = 0x7fffffff;
+const int MaxN = 100010, MaxM = 500010;
+
+struct edge
+{
+    int to, dis, next;
 };
-edge e[MAX<<1];
-int n,m,s;
+
+edge e[MaxM];
+int head[MaxN], dis[MaxN], cnt;
+bool vis[MaxN];
+int n, m, s;
+
+inline void add_edge( int u, int v, int d )
+{
+    cnt++;
+    e[cnt].dis = d;
+    e[cnt].to = v;
+    e[cnt].next = head[u];
+    head[u] = cnt;
+}
+
+struct node
+{
+    int dis;
+    int pos;
+    bool operator <( const node &x )const
+    {
+        return x.dis < dis;
+    }
+};
+
+std::priority_queue<node> q;
+
+
+ void dijkstra()
+{
+    dis[s] = 0;
+    q.push( ( node ){0, s} );
+    while( !q.empty() )
+    {
+        node tmp = q.top();
+        q.pop();
+        int x = tmp.pos, d = tmp.dis;
+        if( vis[x] )
+            continue;
+        vis[x] = 1;
+        for( int i = head[x]; i; i = e[i].next )
+        {
+            int y = e[i].to;
+            if( dis[y] > dis[x] + e[i].dis )
+            {
+                dis[y] = dis[x] + e[i].dis;
+                if( !vis[y] )
+                {
+                    q.push( ( node ){dis[y], y} );
+                }
+            }
+        }
+    }
+}
+
+
 int main()
 {
-	cin >> n >> m >> s;
-	for(int i = 1;i<=m;i++)
-	{
-		int n,t,v,k;
-		cin>>k;
-		cin >> e[i].next >> e[i].to >> e[i].value;
-		head[k] = edge[i];
-	}
-	//ÊäÈë 
-	return 0;
+	std::ios::sync_with_stdio(0);
+    cin >> n >> m >> s;
+    for(int i = 1; i <= n; ++i)
+		dis[i] = INF;
+    for(register int i = 0; i < m; ++i )
+    {
+        register int u, v, d;
+        cin >> u >> v >> d;
+        add_edge( u, v, d );
+    }
+    dijkstra();
+    for( int i = 1; i <= n; i++ )
+        cout << dis[i]<<" ";
+    return 0;
 }
+
