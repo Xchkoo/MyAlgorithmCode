4cf30f05cfc6f82183aab4fd84279b96b215297f
Xchkoo <Xchkoo@foxmail.com>
2020-10-23 19:33:04 +0800

    5 mark

--- luoGuCode/p5657/p5657.cpp
+++ luoGuCode/p5657/p5657.cpp
@@ -1,35 +1,38 @@
 #include<iostream>
 #include<math.h>
 #include<vector>
+#define ull unsigned long long
 using namespace std;
-int n,k,_cnt,number_digit;
-vector<int>ans;
+ull n,k,_cnt,number_digit,s;
+vector<ull>ans;
 void unearth()
 {
-	for(int i = 2;i<=number_digit;i*=2)
+	for(register ull i = 2;i<=number_digit;i*=2)
 	{
-	if((int)k / (_cnt/i) >= 1)
+	if((int)k > s+(_cnt/i))
+	{
+		s = _cnt/i;
 		ans.push_back(1);
+	}
 	else
 		ans.push_back(0);
 	}
 }
 void _give()
 {
-	for(int i = n;i>0;i--)
+	for(register ull i = n;i>0;i--)
 		cout<<ans.back();
 }
 int main()
 {
 	cin >> n >> k;
-	for(int i = 0;i<n;i++)
+	for(register ull i = 0;i<n;i++)
 	{
 		_cnt += (int)pow(double(2),i);//2^i
 	}
 	//a^0 = 1
-	number_digit = n;
-
 	_cnt++;
+	number_digit = n;
 	unearth();
 	_give();
 	return 0;
